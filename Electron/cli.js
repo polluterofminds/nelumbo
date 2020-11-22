9ba0a114ac80a670a85d15b6b9539a526229e320
polluterofminds <justin.edward.hunter@gmail.com>
2020-11-22 10:04:01 -0600

    check dependencies script

--- Electron/cli.js
+++ Electron/cli.js
@@ -1,7 +1,32 @@
-module.exports = {
-  checkOnDependencies: () => {
+const { execSync, exec } = require('child_process');
+
+const checkOnDependencies = async (dependency) => {
+  return new Promise((resolve, reject) => {
     try {
-      console.log("Checking on dependencies")
+      const dep = dependency === 'xcode' ? execSync('xcode-select -p') : execSync(`which ${dependency}`);
+      resolve(dep.toString());
+    } catch (error) {
+      reject(dependency);
+    }
+  })  
+}
+
+module.exports = {
+  checkOnDependencies: async () => {
+    try {
+      const [xcode, git, go, bzr, jq, pkgConfig, rustup, hwloc, lotus] = await Promise.allSettled([
+        checkOnDependencies('xcode'),
+        checkOnDependencies('git'),
+        checkOnDependencies('go'),
+        checkOnDependencies('bzr'),
+        checkOnDependencies('jq'),
+        checkOnDependencies('pkg-config'),
+        checkOnDependencies('rustup'),
+        checkOnDependencies('hwloc'),
+        checkOnDependencies('lotus')
+      ]);
+      const failedDependencies = [xcode, git, go, bzr, jq, pkgConfig, rustup, hwloc, lotus].filter((d) => d.status === 'rejected');
+      return failedDependencies;
     } catch (error) {
       throw error;
     }
